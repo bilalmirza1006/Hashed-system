94f7ae58b262692119615cf600f49c4f9920cf0a
Bilal Mirza <bilal.tech1006@gmail.com>
2024-11-08 11:23:59 +0500

    romove the unused code and commited code

--- src/js/modules/utils.js
+++
@@ -1,9 +0,0 @@
-// Utility functions
-
-/**
- * Returns a random integer between min (inclusive) and max (inclusive).
- */
-export function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-      
